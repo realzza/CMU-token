d11dc6e5d60baf731ee8b6e454515979dde02c71
wshan2 <70585721+wshan2@users.noreply.github.com>
2023-04-24 19:41:35 -0400

    Add files via upload

--- cmu_charger.sol
+++ cmu_charger.sol
@@ -117,10 +117,52 @@ contract StudentChargerSharing is ERC20 {
     }
 
     // Jenny's functions
-    function returnCharger(uint256 chargerId) external { /* ... */ }
-    function rateStudent(address student, uint256 rating) external { /* ... */ }
-    function reportChargerLost(uint256 chargerId) external { /* ... */ }
-    function reportChargerDamaged(uint256 chargerId) external { /* ... */ }
+    function rateStudent(address student, uint256 rating) external { 
+		require(bytes(studentProfiles[msg.sender].name).length != 0, "Student is not registered");
+
+		num_rating = studentProfiles[student].totalRatings;
+		new_rate = (studentProfiles[student].rating * num_rating + rating) / (num_rating + 1);
+	
+		studentProfiles[student].rating = new_rate;
+		studentProfiles[student].totalRatings = num_rating + 1;
+        emit UserRated(msg.sender, student, rating);
+	}
+    
+    
+    function reportChargerLost(uint256 chargerId) external { 
+        price = chargers[chargerId].price;
+        token_price = chargers[chargerId].tokenPrice;
+        
+        if balances[msg.sender] >= price:
+            chargers[chargerId].functional = false;
+            balances[msg.sender] -= price;
+            owner = chargers[chargerId].owner;
+            transferPayment(owner, price / token_price);
+        else:
+            deposit[msg.sender] -= price;
+            owner = chargers[chargerId].owner;
+            transferPayment(owner, price / token_price);
+            
+        emit ChargerReportedLost(chargerId, msg.sender);
+    }
+        
+        
+    function reportChargerDamaged(uint256 chargerId) external { 
+        damageFine = chargers[chargerId].damageFine;
+        token_price = chargers[chargerId].tokenPrice;
+        
+        if balances[msg.sender] >= damageFine:
+            chargers[chargerId].functional = false;
+            balances[msg.sender] -= damageFine;
+            owner = chargers[chargerId].owner;
+            transferPayment(owner, damageFine / token_price);
+        else:
+            deposit[msg.sender] -= damageFine;
+            owner = chargers[chargerId].owner;
+            transferPayment(owner, damageFine / token_price);
+        
+        emit ChargerReportedDamaged(chargerId, msg.sender);
+    }
 
     // Chloris's functions
     function topUpTokens(uint256 amount) external { /* ... */ }
@@ -146,7 +188,6 @@ contract StudentChargerSharing is ERC20 {
         payable(msg.sender).transfer(amount * tokenPrice);
         emit TokensCashedOut(msg.sender, amount);
     }
-}
 
     function rent() external payable {
         require(msg.value >= deposit + rentalFee, "Insufficient funds");
@@ -169,4 +210,3 @@ contract StudentChargerSharing is ERC20 {
         tokens[msg.sender] += refund / tokenPrice;
     }
 }
-
