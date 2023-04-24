47141f14203c50d7cccacc187552f7378e9616c9
wshan2 <70585721+wshan2@users.noreply.github.com>
2023-04-24 19:35:04 -0400

    updated report functions

--- functions.sol
+++ functions.sol
@@ -1,6 +1,5 @@
 function rateStudent(address student, uint256 rating) external { 
 	require(bytes(studentProfiles[msg.sender].name).length != 0, "Student is not registered");
-	// check if the student actually rented the charger
 
 	num_rating = studentProfiles[student].totalRatings;
 	new_rate = (studentProfiles[student].rating * num_rating + rating) / (num_rating + 1);
@@ -13,28 +12,35 @@ function rateStudent(address student, uint256 rating) external {
     
 function reportChargerLost(uint256 chargerId) external { 
 	price = chargers[chargerId].price;
-	require(balances[msg.sender] >= price, "Insufficient balance for charger price");
-	chargers[chargerId].functional = false;
+	token_price = chargers[chargerId].tokenPrice;
 	
-	balances[msg.sender] -= price;
-	owner = chargers[chargerId].owner;
-	transferPayment(owner, price);
-    
+	if balances[msg.sender] >= price:
+		chargers[chargerId].functional = false;
+		balances[msg.sender] -= price;
+		owner = chargers[chargerId].owner;
+		transferPayment(owner, price / token_price);
+	else:
+		deposit[msg.sender] -= price;
+		owner = chargers[chargerId].owner;
+		transferPayment(owner, price / token_price);
+		
     emit ChargerReportedLost(chargerId, msg.sender);
 }
     
     
 function reportChargerDamaged(uint256 chargerId) external { 
 	damageFine = chargers[chargerId].damageFine;
-	require(balances[msg.sender] >= damageFine, "Insufficient balance for damage fine");
-	chargers[chargerId].functional = false;
+	token_price = chargers[chargerId].tokenPrice;
 	
-	balances[msg.sender] -= damageFine;
-	owner = chargers[chargerId].owner;
-	transferPayment(owner, damageFine);
+	if balances[msg.sender] >= damageFine:
+		chargers[chargerId].functional = false;
+		balances[msg.sender] -= damageFine;
+		owner = chargers[chargerId].owner;
+		transferPayment(owner, damageFine / token_price);
+	else:
+		deposit[msg.sender] -= damageFine;
+		owner = chargers[chargerId].owner;
+		transferPayment(owner, damageFine / token_price);
 	
     emit ChargerReportedDamaged(chargerId, msg.sender);
 }
-
-// return: booking.isactive set to inactive?
-// change tokens when transfer fine/charger price
